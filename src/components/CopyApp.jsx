20464d6ead921625ab29427069be2576580cc900
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-05 00:16:57 -0500

    Copy App con useRef terminado

---
+++ src/components/CopyApp.jsx
@@ -0,0 +1,36 @@
+import React, { useRef, useState } from 'react'
+
+const CopyApp = () => {
+
+    const [textInput, setTextInput] = useState('');
+    const [isCopied, setIsCopied] = useState(false);
+    const inputRef = useRef();
+
+    const handleCopy = (e) => {
+        const input = inputRef.current;
+        input.select();
+        document.execCommand('copy');
+        setIsCopied(true);
+        setTimeout(() => {
+            setTextInput('');
+            setIsCopied(false);
+        }, 1500);
+    }
+
+    return (
+        <div className="container">
+            <h2>Copy Input</h2>
+            <input 
+                ref={inputRef}
+                type="text"
+                className="form-control my-3" 
+                placeholder="Type some text here"
+                value={textInput}
+                onChange={(e) => setTextInput(e.target.value)}/>
+            <button className="btn btn-primary" onClick={handleCopy}>Copy!</button>
+            {isCopied && <h3 className="alert alert-success m-3">Copied to clipboard!</h3>}
+        </div>
+    )
+}
+
+export default CopyApp
